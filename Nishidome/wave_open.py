4cb6dec76d3888bcc79218b62e0a1339bc687673
aegis-nishidome <33612342+aegis-nishidome@users.noreply.github.com>
2017-12-15 14:48:11 +0900

    Create wave_open.py

---
+++ Nishidome/wave_open.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    wf = wave.open("pyaudio_output_10mm.wav" , "r" )
+    buf = wf.readframes(wf.getnframes())
+    # バイナリデータを16bit整数に変換
+    data = np.frombuffer(buf, dtype="int16")
+    plt.plot(data)
+    plt.savefig("Wave_10mm.png")          # グラフ保存
+
+if __name__ == '__main__':
+    main()
