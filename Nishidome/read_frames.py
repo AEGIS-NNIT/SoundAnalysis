fd927a0d5ad1339e52eb6dd7842f421510e7aa43
aegis-nishidome <aegis.nishidome@gmail.com>
2018-01-10 16:12:29 +0900

    from domepi

--- Nishidome/read_frames.py
+++ Nishidome/read_frames.py
@@ -2,7 +2,7 @@
 import wave
 
 def main():
-    wf = wave.open("test.wav" , "r" )
+    wf = wave.open("planing.wav" , "r" )
     print"Time[s]:", float(wf.getnframes()) / wf.getframerate()
 
 if __name__ == '__main__':
