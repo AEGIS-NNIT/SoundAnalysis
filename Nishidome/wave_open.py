0d7713b807a942afedb4adeeb13be6c4e44f8144
aegis-nishidome <aegis.nishidome@gmail.com>
2018-01-18 11:29:43 +0900

    from domepi

--- Nishidome/wave_open.py
+++ Nishidome/wave_open.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    wf = wave.open("pyaudio_output_200mm.wav" , "r" ) #読みだしモードでwavファイルを開く
+    wf = wave.open("pyaudio_output_10mm.wav" , "r" ) #読みだしモードでwavファイルを開く
     buf = wf.readframes(wf.getnframes())        #getnframes=wavファイルのオーディオフレーム(サンプリングレート)の総数(サンプリング数)を取得
                                                 #readframes=取得したオーディオフレームの総数分だけのデータを取得し、bitesオブジェクトとして返す
     data = np.frombuffer(buf, dtype="int16")    #バイナリデータを16bit整数に変換
@@ -12,7 +12,7 @@ def main():
     plt.plot(data)                              #描画
     plt.xlabel("Time(プロット数)")
     plt.ylabel("Amplitude()")
-    plt.savefig("Wave_200mm.png")               #グラフ保存
+    plt.savefig("Wave_10mm.png")               #グラフ保存
 
 if __name__ == '__main__':
     main()
